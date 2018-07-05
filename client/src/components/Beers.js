443e1b623bb5af28768d1c5f58a050efdf79ce39
Jace <jacemcgough1@gmail.com>
2018-07-05 13:03:12 -0600

    Tried pagination, switching pages is hard

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -4,10 +4,10 @@ import { Header, Segment, Divider, Grid, Card } from 'semantic-ui-react'
 
 
 class Beers extends React.Component {
-  state = { beers: [] }
+  state = { beers: [], offset: 0 }
 
   componentDidMount() {
-    axios.get('/api/all_beers')
+    axios.get('/api/all_beers', { params: { per_page: 10, page: 1 }})
       .then( res => {
         console.log(res.data.entries)
         this.setState({ beers: res.data.entries })
@@ -38,27 +38,29 @@ class Beers extends React.Component {
   render() {
     
     return(
-      <Segment basic>
-        <Segment basic textAlign='center'>
-          <Header as='h1' style={styles.header}>Some Beer My Dudes</Header>
+      <div>
+        <Segment basic>
+          <Segment basic textAlign='center'>
+            <Header as='h1' style={styles.header}>Some Beer My Dudes</Header>
+          </Segment>
+          <Grid>
+            <Grid.Column computer={16} tablet={16} mobile={16}>
+              <Segment inverted>
+                <Header
+                  as='h1'
+                  textAlign='center'
+                  style={styles.header}>
+                  Beer:
+                </Header>
+                <Divider />
+                <Card.Group stackable itemsPerRow={3}>
+                  {this.displayBeer()}
+                </Card.Group>
+              </Segment>
+            </Grid.Column>
+          </Grid>
         </Segment>
-        <Grid>
-          <Grid.Column computer={16} tablet={16} mobile={16}>
-            <Segment inverted>
-              <Header
-                as='h1'
-                textAlign='center'
-                style={styles.header}>
-                Beer:
-              </Header>
-              <Divider />
-              <Card.Group stackable itemsPerRow={3}>
-                {this.displayBeer()}
-              </Card.Group>
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Segment>
+      </div>
     )
   }
 }
--- notes.md
+++ notes.md
@@ -1,1 +1,3 @@
 Start: 10:53 am -
+
+Can't figure out pagination, but I got the per_page pretty well I think
